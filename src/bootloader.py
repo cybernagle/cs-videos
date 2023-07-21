9e4790936dbb45922e2b1a4f594c5b580cdaf723
Nagle Zhang <1247555725@qq.com>
2023-07-21 12:33:57 +0800

    change bootloader reg from data structure to tabble.

--- src/bootloader.py
+++ src/bootloader.py
@@ -57,22 +57,54 @@ gdtdesc:
                      language="C").scale(0.5)
         self.play(Create(bootloader_16_obj))
 
-        ax_reg = MArray([0,1,0,1,1,1,1,0,0,1,0,1,1,1,1,0]).scale(0.3).shift(LEFT*4)
+        ax_reg = MathTable(
+            [[0,1,0,1,1,1,1,0,0,1,0,1,1,1,1,0]],
+            include_outer_lines = True,
+            include_background_rectangle= True,
+            background_rectangle_color = BLUE,
+            fill_opacity=1,
+        ).scale(0.25).shift(RIGHT*3+UP)
 
-        self.play(Create(ax_reg))
+        #ax_reg = MArray([0,1,0,1,1,1,1,0,0,1,0,1,1,1,1,0]).scale(0.3).shift(LEFT*4)
+        ax_txt = Text("ax", font_size = 15).next_to(ax_reg,LEFT)
+
+        self.play(Create(ax_reg), Create(ax_txt))
         ax_reg_2 = ax_reg.copy()
         self.play(ax_reg_2.animate.shift(DOWN))
 
-        xor = Text("xor", font_size = 20).next_to(ax_reg, DOWN, buff=0.25)
+        xor = Text("xor", font_size = 20).next_to(ax_reg, DOWN, buff=0.3)
         self.play(Create(xor))
 
-        self.play(ax_reg.animate.scale(2).shift(LEFT*2))
-        for i in range(len(ax_reg)-1):
-            self.play(Write(ax_reg.update_elem_value(
-                i, 0, mob_value_args={'color': RED})))#, play_anim=False)))
+        self.play(ax_reg.animate.scale(2).shift(LEFT*2+DOWN*0.5))
+
+        #for i in range(len(ax_reg.get_rows[0].get_entries())):
+        #    print(i)
+
+        for i in ax_reg.get_entries():
+            self.play(Write(i,"0"))
+
 
         #self.play(Write(ax_reg))
-        self.play(ax_reg.animate.shift(RIGHT*2).scale(0.5))
+        self.play(ax_reg.animate.shift(RIGHT*2+UP*0.5).scale(0.5))
+
+        #ds_reg, es_reg, ss_reg  = ax_reg.copy(), ax_reg.copy(), ax_reg.copy()
+
+        #self.play(
+        #    FadeOut(xor),FadeOut(ax_reg_2),
+        #    ds_reg.animate.shift(DOWN*0.5),
+        #    es_reg.animate.shift(DOWN),
+        #    ss_reg.animate.shift(DOWN*1.5),
+        #)
+
+        #ds_txt = Text("ds", font_size = 15).next_to(ds_reg,LEFT)
+        #es_txt = Text("es", font_size = 15).next_to(es_reg,LEFT)
+        #ss_txt = Text("ss", font_size = 15).next_to(ss_reg,LEFT)
+
+        #self.play(
+        #    Create(ds_txt),
+        #    Create(es_txt),
+        #    Create(ss_txt),
+        #)
 
         self.wait()
 
