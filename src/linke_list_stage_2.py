014ee7073106d5b6f478db3ca122ef9c8d89d49b
Nagle Zhang <1247555725@qq.com>
2023-08-06 17:11:49 +0800

    add stage 2, try to visualize free memory.

--- src/linke_list_stage_2.py
+++ src/linke_list_stage_2.py
@@ -8,6 +8,8 @@ class PageLinkedListManagement(MovingCameraScene):#, VoiceoverScene):
     page_link = VGroup()
     pages = VGroup()
     braces = VGroup()
+    point_to_free = DoubleArrow()
+    point_to_prev = DoubleArrow()
 
     def init_pages(self) -> VGroup:
         tables = VGroup(*[
@@ -44,10 +46,10 @@ class PageLinkedListManagement(MovingCameraScene):#, VoiceoverScene):
         self.camera.frame.move_to(self.pages[0].get_center())
         self.set_table_value(self.pages[0], 1, 1, Text("12").set_color(RED))
         self.set_link()
-        self.play(Create(self.page_link))
+        self.add(self.page_link)
         # 这个页链表有12个页
-        self.play(self.camera.frame.animate.scale(8))
-        self.play(self.camera.frame.animate.move_to(self.page_link[5].get_center()))
+        self.camera.frame.scale(8)
+        self.camera.frame.move_to(self.page_link[5].get_center())
         index = 0
         allocations = VGroup()
         for i in range(0, 12, 3):
@@ -59,22 +61,18 @@ class PageLinkedListManagement(MovingCameraScene):#, VoiceoverScene):
             desc = Text("Proces {}".format(i+1)).scale(4).next_to(allocations[i].get_center(), UP, buff=3)
             allocations[i].add(desc)
             allocations[i].save_state()
-            self.play(allocations[i].animate.move_to(allocations[i].get_center() + 6 * UP + i * 2* RIGHT))
-            self.play(allocations[i+1][0].get_rows()[1][1].animate.scale(3))
+            allocations[i].move_to(allocations[i].get_center() + 6 * UP + i * 2* RIGHT)
+            allocations[i+1][0].get_rows()[1][1].scale(3)
             self.set_table_value(allocations[i+1][0], 1, 1, Text(str(12-(i+1)*3)).scale(3).set_color(RED))
 
-        three_orders = [
-            [0,1,2],
-            [2,1,0],
-            [1,2,0],
-        ]
-        for order in three_orders:
-            for i in order:
-                self.play(Restore(allocations[i]), run_time=0.3)
-
-            for i in order:
-                allocations[i].save_state()
-                self.play(allocations[i].animate.move_to(allocations[i].get_center() + 6 * UP + i * 2* RIGHT),run_time=0.3)
-
+        # first order
+        for i in range(3):
+            self.play(Restore(allocations[i]), run_time=1)
+            allocations[i][-3].save_state()
+            # 这个时候, 我们与空闲内存不相邻. 所以我们不能进行合并. 
+            self.play(
+                Indicate(allocations[i][-3]),
+            )
+            # 这样, 我们只能用链表将其串接起来. 但是对 property 我们是不需要进行更新的.
 
         self.wait()
