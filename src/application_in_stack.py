d9c9abd3f5fd426a13fccaf1b988ce48faf7326f
Nagle Zhang <1247555725@qq.com>
2023-09-07 00:36:02 +0800

    finalize application in stack.

--- src/application_in_stack.py
+++ src/application_in_stack.py
@@ -1,8 +1,50 @@
 from manim import *
 
+script = """
+这是一块内存, 黄色区域是用户空间, 蓝色区域是内核空间.
+它们的地址是从 60 - 2-3-0
+
+在1-6-0 到1b0 的位置, 我们存放了应用程序的代码块.
+分别为 main 函数
+launch 函数
+test 函数
+getbuf 函数
+gets 函数
+IO 函数.
+
+其中, main 函数会调用 launch 函数, launch 函数会调用 test 函数, test 会调用 getbuf, getbuf 会调用 gets
+gets,会调用 IO
+
+每个进程在完成其执行后,都会返回到上一个函数.那么它们是怎么继续执行前段函数的代码的呢?
+
+答案是上个函数执行的地址,被存放在了栈当中.
+我们假设 2-3-0 是我们用户栈空间.
+
+当 main 执行到 callq 1-7-0 的时候,其当前执行的地址会被 push 到栈当中.
+换句话说, csrip 所指向的地址, 会被push 到栈当中
+
+这里我们假设 main 执行到了 1-6-5 的位置,跳转到launch函数.
+而在 luanch 函数当中, sub 1-0, rsp 是申请栈空间用于存储参数.
+紧接着 luanch 执行到 callq, test, 或者说, callq 1-8-0 这个地址.同样, 也将当前执行的地址, 我们假设是1-7-8 push 到栈中.随后跳转到
+1-8-0 也就是 test 函数开始执行, test 的逻辑差不多.
+sub 1-0, rsp , 申请栈空间, 然后跳转到 getbuf
+getbuf 申请栈空间, 跳转到 
+gets 再跳转到 IO
+这里的 IO 我们会看到一个长跳转指令, 也就是 相对 rip + 50 的地址上面.
+注意这里的跳转指令只是相对 rip ,而不是 cs rip ,所以它没有用进行用户态到内核态的切换.
+用户态到内核态的切换是在 rip + 50 的这个地址的代码当中的系统调用来进行完成.
+在执行 rip+5-0 的这个系统调用之后, cs rip 将陷入到内核态当中,执行系统调用 IO.
+然后, 程序将按照原路径返回.
+更具体一点, 用户程序从栈中获取到gets的需要继续执行的地址1-a-3, 并跳转过去, 执行完gets函数,从栈中获取到getbuf需要继续执行的地址 1-9-1然后返回到test 地址 183, 接下来都是一样的逻辑, test 再返回到launch地址 178, launch 执行完返回到 main 的 165 的位置.
+
+这样, 我们就完整的完成了整个程序的执行过程.
+最后留一个小问题, 我们在最初的地方提到6-0 到 230 这个地址空间, 是虚拟地址空间, 还是物理地址空间?
+
+"""
 class application_in_stack(Scene):
 
     memory = None
+    address = None
     cs = None
     gdt = None
     stack = None
@@ -96,16 +138,20 @@ class application_in_stack(Scene):
     def create_memory(self) -> VGroup:
         mem = VGroup()
         addresses = VGroup()
-        start_addr = 0x0
+        start_addr = 0x60
         for i in range(30):
             rect = Rectangle(color=BLUE, fill_opacity=0.5, width=0.4, height=1,
                              grid_xstep=0.5, grid_ystep=1)
-            addr = Text(hex(start_addr), font_size = 15)
             rect.next_to(mem, RIGHT, buff=0)
+            addr = Text(hex(start_addr), font_size = 10.5).next_to(rect, DOWN, buff=0.1)
+            start_addr += 0x10
+            addresses.add(addr)
             mem.add(rect)
 
         mem.shift(LEFT*6)
+        addresses.shift(LEFT*6)
         self.memory = mem
+        self.address = addresses
 
     def create_cs(self):
         keys = VGroup(*[Rectangle(height=0.5, width=0.5).set_fill(WHITE, opacity=0.2) for i in range(4)]).arrange(RIGHT, buff=0)
@@ -122,10 +168,9 @@ class application_in_stack(Scene):
         )
         self.wait()
         self.play(
-            self.memory.animate.shift(DOWN*3)
+            self.memory.animate.shift(DOWN*3),
+            self.address.animate.shift(DOWN*3),
         )
-
-
 
         self.play(
             self.memory[16:22].animate.set_color(PURPLE)
@@ -135,22 +180,22 @@ class application_in_stack(Scene):
         self.asm.shift(UP*2.5+LEFT)
 
         self.play(
-            Transform(self.memory[21].copy(),self.main)
+            Transform(self.memory[16].copy(),self.main)
         )
         self.play(
-            Transform(self.memory[20].copy(),self.launch)
+            Transform(self.memory[17].copy(),self.launch)
         )
         self.play(
-            Transform(self.memory[19].copy(),self.test)
+            Transform(self.memory[18].copy(),self.test)
         )
         self.play(
-            Transform(self.memory[18].copy(),self.getbuf)
+            Transform(self.memory[19].copy(),self.getbuf)
         )
         self.play(
-            Transform(self.memory[17].copy(),self.gets)
+            Transform(self.memory[20].copy(),self.gets)
         )
         self.play(
-            Transform(self.memory[16].copy(),self.io)
+            Transform(self.memory[21].copy(),self.io)
         )
 
         indicator = SurroundingRectangle(self.main.code[2])
@@ -160,36 +205,36 @@ class application_in_stack(Scene):
         )
 
         # this just a bunch of shit.
-        reg = Text("cs:rip", color=BLUE).scale(0.5).next_to(self.memory[21],UP)
+        reg = Text("cs:rip", color=BLUE).scale(0.5).next_to(self.memory[16],UP)
         #rbp = Text("rbp", color=BLUE).scale(0.5).next_to(self.stack[1][0],DOWN)
         #rsp = Text("rsp", color=BLUE).scale(0.5).next_to(self.stack[1][0],DOWN)
         self.play(Create(reg))
         self.wait()
-        self.play(reg.animate.next_to(self.memory[20], UP))
-        self.shift_code_indicator(4, indicator, self.launch)
-        self.wait()
-        self.play(reg.animate.next_to(self.memory[19], UP))
-        self.shift_code_indicator(4, indicator, self.test)
+        self.play(reg.animate.next_to(self.memory[17], UP))
+        self.shift_code_indicator(3, indicator, self.launch)
         self.wait()
         self.play(reg.animate.next_to(self.memory[18], UP))
-        self.shift_code_indicator(3, indicator, self.getbuf)
+        self.shift_code_indicator(3, indicator, self.test)
         self.wait()
-        self.play(reg.animate.next_to(self.memory[17], UP))
-        self.shift_code_indicator(3, indicator, self.gets)
+        self.play(reg.animate.next_to(self.memory[19], UP))
+        self.shift_code_indicator(2, indicator, self.getbuf)
         self.wait()
-        self.play(reg.animate.next_to(self.memory[16], UP))
+        self.play(reg.animate.next_to(self.memory[20], UP))
+        self.shift_code_indicator(2, indicator, self.gets)
+        self.wait()
+        self.play(reg.animate.next_to(self.memory[21], UP))
         self.shift_code_indicator(1, indicator, self.io)
         self.wait()
 
-        self.play(reg.animate.next_to(self.memory[17], UP),run_time=0.3)
-        self.shift_code_indicator(3, indicator, self.gets)
-        self.play(reg.animate.next_to(self.memory[18], UP),run_time=0.3)
-        self.shift_code_indicator(3, indicator, self.getbuf, size=1, run_time=0.3)
-        self.play(reg.animate.next_to(self.memory[19], UP),run_time=0.3)
-        self.shift_code_indicator(4, indicator, self.test, size=1, run_time=0.3)
         self.play(reg.animate.next_to(self.memory[20], UP),run_time=0.3)
-        self.shift_code_indicator(4, indicator, self.launch, size=1, run_time=0.3)
-        self.play(reg.animate.next_to(self.memory[21], UP),run_time=0.3)
+        self.shift_code_indicator(2, indicator, self.gets)
+        self.play(reg.animate.next_to(self.memory[19], UP),run_time=0.3)
+        self.shift_code_indicator(2, indicator, self.getbuf, size=1, run_time=0.3)
+        self.play(reg.animate.next_to(self.memory[18], UP),run_time=0.3)
+        self.shift_code_indicator(3, indicator, self.test, size=1, run_time=0.3)
+        self.play(reg.animate.next_to(self.memory[17], UP),run_time=0.3)
+        self.shift_code_indicator(3, indicator, self.launch, size=1, run_time=0.3)
+        self.play(reg.animate.next_to(self.memory[16], UP),run_time=0.3)
         self.shift_code_indicator(2, indicator, self.main, size=1, run_time=0.3)
 
         # 返回的时候, 要怎么工作呢?
@@ -202,22 +247,125 @@ class application_in_stack(Scene):
         #kernel_stack = self.stack.copy().shift(UP*2+RIGHT*3).set_color(BLUE)
         self.play(self.stack.next_to(self.memory[29]).animate.shift(UP*4+LEFT*12))
 
-        # 首先我们在栈底记
+        # 首先在栈底, 是我们的 main function 
+        one = Text("0x165").scale(0.5).move_to(self.stack[0][-1])
+        self.play(
+            self.stack[0][-1].animate.set_color(PURPLE_A),
+            Create(one)
+        )
 
-        self.play(reg.animate.next_to(self.memory[20], UP))
-        self.shift_code_indicator(4, indicator, self.launch)
-        self.wait()
-        self.play(reg.animate.next_to(self.memory[19], UP))
-        self.shift_code_indicator(4, indicator, self.test)
-        self.wait()
-        self.play(reg.animate.next_to(self.memory[18], UP))
-        self.shift_code_indicator(3, indicator, self.getbuf)
-        self.wait()
+        # 然后, main callq launch
+        # 紧接着执行指令, sub rsp 会在
         self.play(reg.animate.next_to(self.memory[17], UP))
-        self.shift_code_indicator(3, indicator, self.gets)
-        self.wait()
-        self.play(reg.animate.next_to(self.memory[16], UP))
-        self.shift_code_indicator(1, indicator, self.io)
+        self.shift_code_indicator(1, indicator, self.launch)
+        self.play(
+            self.stack[0][-2].animate.set_color(PURPLE_A)
+        )
+
+        self.shift_code_indicator(3, indicator, self.launch)
+        two  = Text("0x178").scale(0.5).move_to(self.stack[0][-3])
+        self.play(
+            self.stack[0][-3].animate.set_color(PURPLE_A),
+            Create(two)
+        )
+
+        # execute test
+        self.play(reg.animate.next_to(self.memory[18], UP))
+        self.shift_code_indicator(1, indicator, self.test)
+        self.play(
+            self.stack[0][-4].animate.set_color(PURPLE_A)
+        )
+        self.shift_code_indicator(3, indicator, self.test)
+        three = Text("0x183").scale(0.5).move_to(self.stack[0][-5])
+        self.play(
+            self.stack[0][-5].animate.set_color(PURPLE_A),
+            Create(three)
+        )
         self.wait()
 
+        # execute getbuf
+        self.play(reg.animate.next_to(self.memory[19], UP))
+        self.shift_code_indicator(1, indicator, self.getbuf)
+        self.play(
+            self.stack[0][-6].animate.set_color(PURPLE_A)
+        )
+
+        self.shift_code_indicator(2, indicator, self.getbuf)
+        four = Text("0x191").scale(0.5).move_to(self.stack[0][-7])
+        self.play(
+            self.stack[0][-7].animate.set_color(PURPLE_A),
+            Create(four)
+        )
         self.wait()
+
+        # execute gets
+        self.play(reg.animate.next_to(self.memory[20], UP))
+        self.shift_code_indicator(2, indicator, self.gets)
+        five = Text("0x1a3").scale(0.5).move_to(self.stack[0][-8])
+        self.play(
+            self.stack[0][-8].animate.set_color(PURPLE_A),
+            Create(five)
+        )
+        self.wait()
+
+        # execute IO
+        self.play(reg.animate.next_to(self.memory[21], UP))
+        self.shift_code_indicator(1, indicator, self.io)
+
+        self.play(self.memory[26].animate.set_color(RED_C))
+        self.play(reg.animate.next_to(self.memory[26], UP))
+
+        # trap into kernel space
+        self.play(self.memory[5].animate.set_color(RED_C))
+        self.play(reg.animate.next_to(self.memory[5], UP))
+
+        # return from kernel space
+        self.wait()
+        self.play(reg.animate.next_to(self.memory[26], UP),run_time=0.3)
+        self.play(reg.animate.next_to(self.memory[21], UP),run_time=0.3)
+
+        # return to gets
+        self.play(
+            self.stack[0][-8].animate.set_color(RED),
+            FadeOut(five)
+        )
+        self.play(reg.animate.next_to(self.memory[20], UP),run_time=0.3)
+        self.shift_code_indicator(3, indicator, self.gets, size=1, run_time=0.3)
+
+        # return to getbuf
+        self.play(
+            self.stack[0][-7].animate.set_color(RED),
+            FadeOut(four)
+        )
+        self.play(reg.animate.next_to(self.memory[19], UP),run_time=0.3)
+        self.shift_code_indicator(3, indicator, self.getbuf, size=1, run_time=0.3)
+
+        # return to test
+        self.play(
+            self.stack[0][-5].animate.set_color(RED),
+            self.stack[0][-6].animate.set_color(RED),
+            FadeOut(three)
+        )
+        self.play(reg.animate.next_to(self.memory[18], UP),run_time=0.3)
+        self.shift_code_indicator(4, indicator, self.test, size=1, run_time=0.3)
+
+        # return to launch
+        self.play(
+            self.stack[0][-3].animate.set_color(RED),
+            self.stack[0][-4].animate.set_color(RED),
+            FadeOut(two)
+        )
+        self.play(reg.animate.next_to(self.memory[17], UP),run_time=0.3)
+        self.shift_code_indicator(4, indicator, self.launch, size=1, run_time=0.3)
+
+        # return to main
+        self.play(
+            self.stack[0][-1].animate.set_color(RED),
+            self.stack[0][-2].animate.set_color(RED),
+            FadeOut(one)
+        )
+        self.play(reg.animate.next_to(self.memory[16], UP),run_time=0.3)
+        self.shift_code_indicator(3, indicator, self.main, size=1, run_time=0.3)
+
+        self.wait()
+
--- src/character.py
+++ src/character.py
@@ -4,17 +4,25 @@ class Characters(Scene):
     face = None
 
     def create_face(self):
-        face = Text(":-)")
-        surrand= SurroundingRectangle(face, color=WHITE)
-        surranded_face = VGroup(face, surrand)
-        surranded_face.scale(2)#.rotate(1.5*PI)
-        self.face = surranded_face
+        face = Text(":-|").set_color(YELLOW).scale(2).rotate(1.5*PI)
+        #surrand= SurroundingRectangle(face, color=WHITE,corner_radius=0.5)
+        #surranded_face = VGroup(face, surrand)
+        #surranded_face.scale(2).rotate(1.5*PI)
+        self.face = face #surranded_face
 
     def construct(self):
         self.create_face()
-        self.play(Create(self.face))
-        #self.play(
-        self.face[0].become(Text(":-(").scale(2))#.rotate(1.5*PI))
+        self.add(self.face)
+        self.play(
+            self.face.animate.become(Text(":-o").scale(2).rotate(1.5*PI).set_color(YELLOW)),
+        )
+        self.wait()
+        self.play(
+            self.face.animate.become(Text(":-)").scale(2).rotate(1.5*PI).set_color(YELLOW)),
+        )
+        self.wait()
 
-        #)
+        title = Text("muMu").next_to(self.face, DOWN, buff=0.3).set_color(GRAY)
+        self.play(FadeIn(title))
+
         self.wait()
