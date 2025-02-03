bf9bbb6814e7ff4de2a11914fbfe2091cf6d1b32
Nagle Zhang <nagle.zhang@qq.com>
2025-02-03 20:06:29 +0800

    feat: finalize.

--- src/20250105.quant-engineer/main.py
+++ src/20250105.quant-engineer/main.py
@@ -4,6 +4,9 @@ from manim_voiceover import VoiceoverScene
 from manim.scene.moving_camera_scene import MovingCameraScene
 from manim_voiceover.services.azure import AzureService
 
+
+# import numpy as np
+
 # below code did not work
 # import manimforge as mf
 # mf.setup()
@@ -21,6 +24,7 @@ SHADOW="#D3D3D3"
 
 
 class QuantitativeEngineer(MovingCameraScene, VoiceoverScene):
+    labels = VGroup()
 
     def computer_voice(self):
         self.set_speech_service(
@@ -40,38 +44,69 @@ class QuantitativeEngineer(MovingCameraScene, VoiceoverScene):
 
     def curve(self):
         # 创建坐标轴
-        axes = Axes(
-            x_range=[0, 10, 1],
-            y_range=[0, 4, 0.5],
-            axis_config={
-                "include_numbers": False,
-                "color": DARK_GRAY
-            },
-            color = WORD_A
+        # 创建二维坐标系，坐标从0开始
+        axes = NumberPlane(
+            x_range=[0, 30, 5],  # x轴范围从0到30
+            y_range=[0, 10000, 1000],  # y轴范围从0到10000
+            x_length=10,        # x轴长度
+            y_length=6,         # y轴长度
+            background_line_style={
+                "stroke_color": LIGHT_GRAY ,
+                "stroke_width": 4,
+                "stroke_opacity": 0.6
+            }
         )
+        self.add(axes)
 
-        # 定义模拟指数基金回报的函数
         def fund_return_function(x):
-            return 0.1 * (x - 2) * (x - 6) * (x - 8) / 5 + 2
+            np.random.seed(int(x * 10))  # 根据 x 值设置随机种子，使函数具有随机性
+            base_value = 9000 / (np.exp(0.1 * 30) * (1 + 0.1 * np.sin(10) + 0.05 * np.sin(50) + 0.05 * np.sin(100)))
+            return base_value * np.exp(0.1 * x) * (1 + 0.1 * np.sin(x) + 0.05 * np.sin(5 * x) + 0.05 * np.sin(10 * x))
+
+        def customize_portfolio(x):
+            # 根据 x 值设置随机种子，使函数具有随机性
+            np.random.seed(int(x * 10))
+            # 增加正弦项的幅度以增加波动性
+            amplitude_factor = 3  # 这个因子可以调整波动的幅度
+            # 定义原始函数
+            original_value = np.exp(0.1 * x) * (1 + amplitude_factor * 0.1 * np.sin(x) + amplitude_factor * 0.05 * np.sin(5 * x) + amplitude_factor * 0.05 * np.sin(10 * x))
+            # 计算在 x = 30 时的修正因子
+            correction_factor = 6000 / (np.exp(0.1 * 30) * (1 + amplitude_factor * 0.1 * np.sin(30) + amplitude_factor * 0.05 * np.sin(150) + amplitude_factor * 0.05 * np.sin(300)))
+            # 返回修正后的函数值
+            return correction_factor * original_value
+
+        def your_portfolio(x):
+            np.random.seed(int(x * 10))  # 根据 x 值设置随机种子，使函数具有随机性
+            base_value = 6000 / (np.exp(0.1 * 30) * (1 + 0.1 * np.sin(10) + 0.05 * np.sin(50) + 0.05 * np.sin(100)))
+            return base_value * np.exp(0.1 * x) * (1 + 0.1 * np.sin(x) + 0.05 * np.sin(5 * x) + 0.05 * np.sin(10 * x))
 
         # 绘制函数曲线
+
         fund_return_curve = axes.plot(fund_return_function, color=BLUE)
+        customize_curve = axes.plot(customize_portfolio, color=PURPLE)
+        index_curve_pos = axes.c2p(30, fund_return_function(30))
+        customize_curve_pos = axes.c2p(30, customize_portfolio(30))
 
         # 添加坐标轴和曲线到场景
         self.add(axes, fund_return_curve)
 
-        # 添加标题
-        title = Text("指数基金回报曲线", font_size=36, color=OBJ_A).to_edge(UP)
-        self.add(title)
+        index_title = Text(
+            "指数基金回报", font_size=20, color=BLUE
+        ).move_to(index_curve_pos).shift(DOWN*0.4)
+        customize_title = Text(
+            "个人组合回报", font_size=20, color=PURPLE
+        ).move_to(customize_curve_pos).shift(DOWN*0.4)
 
-        # 显示动画
-        self.play(Create(axes), Create(fund_return_curve), Write(title))
-        self.wait(2)
+        sp500 = "大部分人应该都听过指数基金这个东西. 如标普500指数基金，包含了500家大型的不同行业公司的股票，能够反映整体市场的表现, 甚至说经济的表现."
 
-    def portfolio(self):
+        with self.voiceover(text=sp500) as tracker:
+            self.play(Create(axes), Create(fund_return_curve))
+            self.play(Write(index_title))
 
-        def fund_return_function(x):
-            return 0.05 * (x - 2) * (x - 6) * (x - 8) * np.sin(5 * x)
+        portfolio_text = "量化工程师将使用指数基金作为基准, 表现优于指数, 我们说它打败了市场.否则, 它就是被市场击败."
+        with self.voiceover(text=portfolio_text) as tracker:
+            self.play(Create(customize_curve))
+            self.play(Write(customize_title))
 
     def portfolio_instance(self):
 
@@ -82,28 +117,27 @@ class QuantitativeEngineer(MovingCameraScene, VoiceoverScene):
         microsoft = ImageMobject(f"{IMAGE}/logo-microsoft.png").scale(0.3)
         netflix = ImageMobject(f"{IMAGE}/logo-netflix.png").scale(0.3)
 
-        # 图像列表
         images = [tiktok, amazon, facebook, google, microsoft, netflix]
 
-        # 创建图像组
-        image_group = Group(*images
+        image_group = Group(
+            *images
         ).arrange(RIGHT, buff=0.5).move_to(UP * 1.5)
 
-        # 添加图像到场景
-        self.play(FadeIn(image_group))
+        pick = "作为量化工程师, 将首先选择自己的价值组合."
+        with self.voiceover(text=pick) as tracker:
+            self.play(FadeIn(image_group))
 
         x_labels = [MathTex(f"x_{{{i+1}}}", color=BLACK) for i in range(len(images))]
 
-        # 将每个标签放置在对应的图像下方
         for image, label in zip(image_group, x_labels):
-            label.next_to(image, DOWN, buff=0.5)
+            label.next_to(image, DOWN, buff=2)
 
-        # 将标签组合成一个 VGroup
         x_labels_group = VGroup(*x_labels)
 
-        self.play(FadeIn(x_labels_group))
+        quantlize = "随后将其量化为 x1 到 xn."
+        with self.voiceover(text=quantlize) as tracker:
+            self.play(FadeIn(x_labels_group))
 
-        # 创建 b1, b2, b3, b4, b5, b6 标签
         b_labels = [MathTex(f"b_{i+1}", color=RED) for i in range(len(images))]
 
         for b, x in zip(b_labels, x_labels):
@@ -113,41 +147,47 @@ class QuantitativeEngineer(MovingCameraScene, VoiceoverScene):
 
         alpha_label = MathTex(f"alpha  +", color=RED).next_to(b_labels_group, LEFT, buff=0.1)
 
-        # 创建加号
         plus_signs = [MathTex("+", color=RED) for _ in range(len(images) - 1)]
         for p, x in zip(plus_signs, x_labels):
             p.next_to(x, RIGHT, buff=0.2)
         plus_signs_group = VGroup(*plus_signs)
 
-        self.play(
-            FadeIn(b_labels_group),
-            FadeIn(alpha_label),
-            FadeIn(plus_signs_group)
-        )
-
-
         equal_sign = MathTex("=").next_to(x_labels_group, RIGHT, buff=0.2)
         y_label = Text(r"= Y", color=RED).scale(0.8).next_to(x_labels_group, RIGHT, buff=0.2)
         index_label = Text(r"= 指数基金(sp500)", color=RED).scale(0.45).next_to(x_labels_group, RIGHT, buff=0.2)
-        self.play(FadeIn(y_label))
-        self.play(FadeOut(y_label))
-        self.play(FadeIn(index_label))
+
+        quant_index = "而基准指数的回报, 我们将其设为 y, 随后为自己的组合添加上权重 beta, 和 alpha"
+        with self.voiceover(text=quant_index) as tracker:
+            self.play(
+                FadeIn(b_labels_group),
+                FadeIn(alpha_label),
+                FadeIn(plus_signs_group),
+                FadeIn(y_label)
+            )
+
+
+        final = "这里的指数 Y 可以是指代我们前面提到的标普500"
+        with self.voiceover(text=final) as tracker:
+            self.play(FadeOut(y_label))
+            self.play(FadeIn(index_label))
+
+        defeat = "这里面的 alpha 为负时,  我们可以说组合被市场击败, 否则, 我们就击败了市场."
+        with self.voiceover(text=defeat) as tracker:
+            self.play(Indicate(alpha_label))
+
+        self.labels.add(x_labels_group, b_labels_group, index_label, plus_signs_group, alpha_label)
 
         self.wait(2)
 
     def compute_alpha(self):
         # Create the computer image
         computer = ImageMobject(f"{IMAGE}/laptop.png").scale(0.8)
-        self.play(FadeIn(computer))
+        linear_regression = "紧接着, 我们将公式进行线性回归运算."
+        with self.voiceover(text=linear_regression) as tracker:
+            self.play(FadeIn(computer))
 
-        # Create the text below the computer
-        linear_regression_text = MathTex(
-            r"\text{linear regression: } \alpha + b_1x_1 + b_2x_2 + b_3x_3 + \dots + b_nx_n",
-            color=BLACK
-        ).scale(0.8).next_to(computer, DOWN, buff=0.5)
-        self.play(
-            FadeIn(linear_regression_text)
-        )
+            self.labels.next_to(computer, DOWN, buff=0.5)
+            self.play(FadeIn(self.labels))
 
         # Create historical data text on the left
         historical_data = Text("历史数据", font_size=30, color=BLACK).shift( LEFT * 5 )
@@ -157,22 +197,27 @@ class QuantitativeEngineer(MovingCameraScene, VoiceoverScene):
             buff=0.5, color=BLACK
         )
 
-        self.play(
-            FadeIn(historical_data),
-            FadeIn(arrow_to_computer)
-        )
+        history = "代入历史数据以后"
+
+        with self.voiceover(text=history) as tracker:
+            self.play(
+                FadeIn(historical_data),
+                FadeIn(arrow_to_computer)
+            )
 
         # Create an arrow from the computer to the right
         arrow_to_right = Arrow(start=computer.get_right(), end=RIGHT * 5, buff=0.5, color=BLACK)
-        alpha_label = MathTex(r"\alpha", color=BLACK).scale(0.8).next_to(
+        alpha_label = MathTex(r"alpha", color=RED).scale(0.8).next_to(
             arrow_to_right,RIGHT , buff=0.5
         ).shift(UP*0.2)
-        beta_label = MathTex(r"\beta", color=BLACK).scale(0.8).next_to(
+        beta_label = MathTex(r"beta", color=RED).scale(0.8).next_to(
             arrow_to_right, RIGHT, buff=0.5
         ).shift(DOWN*0.2)
 
-        self.play(Write(arrow_to_right))
-        self.play(FadeIn(alpha_label),FadeIn(beta_label))
+        ab = "我们就拥有了 alpha 和 beta"
+        with self.voiceover(text=ab) as tracker:
+            self.play(Write(arrow_to_right))
+            self.play(FadeIn(alpha_label),FadeIn(beta_label))
 
     def alpha_perf(self):
         alpha = MathTex(r"\alpha", font_size=144, color=BLACK)
@@ -185,27 +230,109 @@ class QuantitativeEngineer(MovingCameraScene, VoiceoverScene):
             r" \dots "
             r"\end{cases}", color=BLACK
         )
-        self.play(Write(equation))
+        perf = "得到了 alpha 以后, 需要从我们之前的方程式提取出更加细节的东西, 也被称之为 alpha 的性能. 比方说,上下波动是否很大?这个东西被称之为波动率; 之前买的人多吗?被称之为动量; 最近加息降息了吗?被称之为宏观因子; 等等等等, 最后, 我们就可以使用机器来将各个因子代入, 并且进行统计学习并根据其进行预测."
+        with self.voiceover(text=perf) as tracker:
+            self.play(Write(equation))
+
+    def effect(self):
+        # 创建二维坐标系，坐标从0开始
+        ax = NumberPlane(
+            x_range=[0, 30, 5],  # x轴范围从0到30
+            y_range=[0, 10000, 1000],  # y轴范围从0到10000
+            x_length=10,        # x轴长度
+            y_length=6,         # y轴长度
+            background_line_style={
+                "stroke_color": LIGHT_GRAY ,
+                "stroke_width": 4,
+                "stroke_opacity": 0.6
+            }
+        )
+        self.add(ax)
+
+        # 定义随机波动函数
+        def random_wave_func(x):
+            np.random.seed(int(x * 10))  # 根据 x 值设置随机种子，使函数具有随机性
+            base_value = 2000 / (np.exp(0.1 * 10) * (1 + 0.1 * np.sin(10) + 0.05 * np.sin(50) + 0.05 * np.sin(100)))
+            return base_value * np.exp(0.1 * x) * (1 + 0.1 * np.sin(x) + 0.05 * np.sin(5 * x) + 0.05 * np.sin(10 * x))
+
+        def exp2(x):
+            # 定义原始函数
+            original_value = np.exp(0.2 * x) * (1 + 0.1 * np.sin(x) + 0.05 * np.sin(5 * x) + 0.05 * np.sin(10 * x))
+            # 计算修正因子
+            correction_factor_10 = 2000 / (np.exp(0.2 * 10) * (1 + 0.1 * np.sin(10) + 0.05 * np.sin(50) + 0.05 * np.sin(100)))
+            correction_factor_20 = 6000 / (np.exp(0.2 * 20) * (1 + 0.1 * np.sin(20) + 0.05 * np.sin(100) + 0.05 * np.sin(200)))
+            if x <= 10:
+                return correction_factor_10 * original_value
+            else:
+                return correction_factor_20 * original_value
+
+        def exp3(x):
+            if x <= 20:
+                return 6000
+            elif x <= 25:
+                # 指数下降
+                return 6000 * np.exp(-0.2 * (x - 20))
+            else:
+                # 随机运动
+                np.random.seed(int(x * 10))  # 为了使随机结果可重复
+                return 4000 + 1000 * np.random.uniform(-1, 1)
+
+            # 定义原始函数
+            pass
+
+
+        step1 = "所以我们也可以看到, 当一些宏观的数据出现的时候, 市场就会有比较大的波动."
+        wave_graph = ax.plot(random_wave_func, x_range=[0, 10], color=BLUE)
+
+        with self.voiceover(text=step1) as tracker:
+            self.play(Create(wave_graph), run_time=tracker.duration)
+
+        end_point1 = ax.c2p(10, random_wave_func(10))
+        macro = Text("联储降息", color=WORD_A).scale(0.5).move_to(end_point1).shift(UP*0.2)
+        step2 = "比如说, 联储的降息, 就会引起很大的数据变动."
+
+        with self.voiceover(text=step2) as tracker:
+            self.play(FadeIn(macro))
+            exp_graph = ax.plot(exp2, x_range=[10, 20], color=GREEN)
+            self.play(Create(exp_graph))
+
+        end_point2 = ax.c2p(20, exp2(20))
+        report = Text("Deepseek 火了", color=RED).scale(0.5).move_to(end_point2).shift(UP*0.2)
+
+        step3 = "最近的 deepseek 的模型发布, 也导致了价格发生了很大的变动.这些, 有很大的部分都是因为将因子代入到公式以后, 触发了买卖操作而导致."
+        with self.voiceover(text=step3) as tracker:
+            self.play(FadeIn(report))
+            exp_graph = ax.plot(exp3, x_range=[20, 30], color=BLUE)
+            self.play(Create(exp_graph))
+
 
     def construct(self):
         self.human_voice()
 
-        """
-        people = OpenGLImageMobject(f"{IMAGE}/people-head-00.png").shift(UP)
-        # people = ImageMobject(f"{IMAGE}/people-head-00.png").shift(UP)
+        people = ImageMobject(f"{IMAGE}/people-head-00.png").scale(0.7)
+        stockmarket = ImageMobject(f"{IMAGE}/stock-market.png").scale(0.7)
         scene1 = "我们知道, 量化工程师无非是要选择买或者卖股票, 那么, 这个过程中,他们需要量化的是什么呢?"
 
-        # with self.voiceover(text=scene1) as tracker:
-        self.play(FadeIn(people))
-        people.shift(RIGHT*5)
-        # self.wait(10)
-        #  self.vo
+        with self.voiceover(text=scene1) as tracker:
+            self.play(FadeIn(people))
+            self.play(people.animate.shift(LEFT*3))
 
-        index = Text("指数基金", color=WORD_A)
-        self.play(FadeIn(index))
-        self.interactive_embed()
-        """
-        # self.curve()
-        # self.portfolio_instance()
-        # self.compute_alpha()
+            self.play(FadeIn(stockmarket))
+            self.play(stockmarket.animate.shift(RIGHT*3))
+            arrow_to_stock = Arrow(
+                start=people.get_right(),
+                end=stockmarket.get_left(),
+                buff=0.5, color=BLACK
+            )
+            self.play(Write(arrow_to_stock))
+
+        self.clear()
+        self.curve()
+        self.clear()
+        self.portfolio_instance()
+        self.clear()
+        self.compute_alpha()
+        self.clear()
         self.alpha_perf()
+        self.clear()
+        self.effect()
